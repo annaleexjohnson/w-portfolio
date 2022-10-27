8ed2e24cb85c92407a574a0b8c909431d4cc3b3f
Annalee X Johnson <annaleexjohnson@gmail.com>
2022-10-27 15:03:58 -0500

    Add footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+
+    const style={
+        width: "100%",
+        backgroundColor: "#606C38",
+        color: "white",
+        padding: "0.5rem 1rem",
+        fontSize: "0.8rem"
+    }
+  return (
+    <div style={style}>
+        <span>Website designed and created by Annalee X. Johnson with React & Vite | 2022</span>
+    </div>
+  )
+}
+
+export default Footer
